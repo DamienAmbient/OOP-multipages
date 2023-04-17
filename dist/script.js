97b5741f2f7f38308cb3d9738421d33c8e79f3cd
Damien <damienambient@gmail.com>
2023-04-17 19:20:51 +0300

    created videoplayer step2: YT player

--- dist/script.js
+++ dist/script.js
@@ -103,7 +103,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const slider = new _modules_slider__WEBPACK_IMPORTED_MODULE_0__["default"](".page", ".next");
   slider.render();
   const player = new _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__["default"](".showup .play", ".overlay");
-  player.play();
+  player.init();
 });
 
 /***/ }),
@@ -124,12 +124,36 @@ class VideoPlayer {
     this.overlay = document.querySelector(overlay);
     this.close = this.overlay.querySelector(".close");
   }
-  play() {
+  createPlayer(url) {
+    this.player = new YT.Player("frame", {
+      height: "100%",
+      width: "100%",
+      videoId: `${url}`
+    });
+    console.log(this.player);
+    this.overlay.style.display = "flex";
+  }
+  bindTriggers() {
     this.btns.forEach(btn => {
       btn.addEventListener("click", () => {
-        console.log("hello");
+        const path = btn.getAttribute("data-url");
+        this.createPlayer(path);
+        this, player.stopVideo();
       });
     });
+  }
+  bindCloseBtn() {
+    this.close.addEventListener("click", () => {
+      this.overlay.style.display = "none";
+    });
+  }
+  init() {
+    const tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    this.bindTriggers();
+    this.bindCloseBtn();
   }
 }
 
--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -5,11 +5,41 @@ export default class VideoPlayer {
         this.close = this.overlay.querySelector(".close");
     }
 
-    play() {
+    createPlayer(url) {
+        this.player = new YT.Player("frame", {
+            height: "100%",
+            width: "100%",
+            videoId: `${url}`,
+        });
+
+        console.log(this.player);
+        this.overlay.style.display = "flex";
+    }
+
+    bindTriggers() {
         this.btns.forEach((btn) => {
             btn.addEventListener("click", () => {
-                console.log("hello");
+                const path = btn.getAttribute("data-url");
+                this.createPlayer(path);
+                this, player.stopVideo();
             });
         });
+    }
+
+    bindCloseBtn() {
+        this.close.addEventListener("click", () => {
+            this.overlay.style.display = "none";
+        });
+    }
+
+    init() {
+        const tag = document.createElement("script");
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        const firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        this.bindTriggers();
+        this.bindCloseBtn();
     }
 }
